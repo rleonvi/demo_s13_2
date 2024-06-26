47eac15ba4423847b105bef7074550b7c6d4c579
Rony Leon <rleonvi15@gmail.com>
2024-06-25 20:35:50 -0500

    subida prueba 2

---
+++ funciones.js
@@ -0,0 +1,16 @@
+//funciones
+
+	function ocultar_ventana()
+	{
+		//capturamos la ventana modal
+		var contendormodal= document.getElementById("contenedor_ventana_modal").style.display = "none";
+		
+	}
+	
+	//funcion que muestra ventana modal
+	function mostrar_ventana()
+	{
+		//capturamos la ventana modal
+		var contendormodal= document.getElementById("contenedor_ventana_modal").style.display = "block";
+		
+	}
